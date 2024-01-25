43e8e9ca5c6573c7712059105207451e8fa2904c
Fernando Moutinho <moutinho132@gmail.com>
2024-01-25 11:12:35 -0400

    Mi primer commit

---
+++ src/main/java/com/example/practica/MiPracticaGit2Application.java
@@ -0,0 +1,13 @@
+package com.example.practica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiPracticaGit2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiPracticaGit2Application.class, args);
+    }
+
+}
